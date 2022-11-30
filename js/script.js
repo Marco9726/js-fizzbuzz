e36d6879405f0d19e42c042426ed41768a8e613d
Marco 9726 <marcosarra9@gmail.com>
2022-11-30 16:43:09 +0100

    fine stil. e ottimizzazione ciclo

--- js/script.js
+++ js/script.js
@@ -2,27 +2,29 @@ const container = document.querySelector('.container')
 
 //1
 for (let i=1; i<=100; i++){
-    //2
-    if ( i % 3 == 0 && i % 5 != 0)
-    {
-        let word = 'Fizz' 
+    //4
+    if ( i % 15 == 0 )
+     {
+        let word = 'Fizzbuzz'
         console.log(word);
-        //5.1
-        container.innerHTML += `<div class="square square-fizz">${word}</div>`
+        //5.3
+        container.innerHTML += `<div class="square square-fizzbuzz">${word}</div>`
     }
     //3
-    else if ( i % 3 != 0 && i % 5 == 0 ){
+    else if ( i % 5 == 0 )
+    {
         let word = 'Buzz'
         console.log(word);
         //5.2
         container.innerHTML += `<div class="square square-buzz">${word}</div>`
     }
-    //4
-    else if ( i % 15 == 0 ) {
-        let word = 'Fizzbuzz'
+    //2
+    else if ( i % 3 == 0 )
+    {
+        let word = 'Fizz' 
         console.log(word);
-        //5.3
-        container.innerHTML += `<div class="square square-fizzbuzz">${word}</div>`
+        //5.1
+        container.innerHTML += `<div class="square square-fizz">${word}</div>`
     }
     else {
         let word = i
