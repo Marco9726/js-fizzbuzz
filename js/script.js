dd6127e39e0fa672a783a8b4fd4b1384c1687379
Marco 9726 <marcosarra9@gmail.com>
2022-11-30 15:46:20 +0100

    visualizzazionei valori in elementi html

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
+const container = document.querySelector('.container')
+
 //1
 for (let i=1; i<=100; i++){
+    //2
     if ( i % 3 == 0 && i % 5 != 0)
     {
-        console.log('Fizz');
+        let word = 'Fizz' 
+        console.log(word);
+        //5.1
+        container.innerHTML += `<div class="square square-fizz">${word}</div>`
     }
-
+    //3
     else if ( i % 3 != 0 && i % 5 == 0 ){
-        console.log('Buzz');
+        let word = 'Buzz'
+        console.log(word);
+        //5.2
+        container.innerHTML += `<div class="square square-buzz">${word}</div>`
     }
-
+    //4
     else if ( i % 15 == 0 ) {
-        console.log('FizzBuzz');
+        let word = 'Fizzbuzz'
+        console.log(word);
+        //5.3
+        container.innerHTML += `<div class="square square-fizzbuzz">${word}</div>`
     }
-
     else {
-        console.log(i);
+        let word = i
+        console.log(word);
+        //5
+        container.innerHTML += `<div class="square">${word}</div>`  
     }
 }
+
